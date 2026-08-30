aae8577b032ea2d9fd8309d6b6097b3f87ec93ab
Prapti2006 <praptimaity9b@gmail.com>
2026-08-30 22:32:02 +0530

    Remove Member 1 files from main

--- Member 1/list_voices.ps1
+++
@@ -1,7 +0,0 @@
-Add-Type -AssemblyName System.Speech
-$synth = New-Object System.Speech.Synthesis.SpeechSynthesizer
-$voices = $synth.GetInstalledVoices()
-foreach ($v in $voices) {
-    Write-Host ($v.VoiceInfo.Name + " | " + $v.VoiceInfo.Gender + " | " + $v.VoiceInfo.Culture + " | " + $v.VoiceInfo.Age)
-}
-$synth.Dispose()
--- Member 1/model/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Model package for EchoForge Member 1.
-"""
-
-from .detector import Wav2Vec2DeepfakeDetector, get_detector
-
-__all__ = ["Wav2Vec2DeepfakeDetector", "get_detector"]
