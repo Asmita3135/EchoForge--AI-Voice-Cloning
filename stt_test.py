3c7a3d6019e3fd0129196deeb13413cccff5c687
Sagnik Majumder <coder.op.sagnik@gmail.com>
2026-08-31 20:39:41 +0530

    Revert "Added context analysis module"

--- stt_test.py
+++
@@ -1,8 +0,0 @@
-import whisper  # type: ignore[import-untyped]
-
-model = whisper.load_model("tiny")
-
-result = model.transcribe("audio/Training.wav")
-
-print("TRANSCRIPT:")
-print(result["text"])
